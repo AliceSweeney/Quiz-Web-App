544f2a861aef9ffc93eb83bdd3b9502ddab231e2
AliceSweeney <alice@sweeneyclan.org>
2025-09-04 13:30:08 -0700

    Refactor: shorten jquery code using traversing

--- script.js
+++ script.js
@@ -1,35 +1,5 @@
 $(document).ready(function() {
-	$("#q1").click(function(){
-		$("#a1").css("visibility", "visible");
-	});
-	$("#q2").click(function(){
-		$("#a2").css("visibility", "visible");
-	});
-	$("#q3").click(function(){
-		$("#a3").css("visibility", "visible");
-	});
-	$("#q4").click(function(){
-		$("#a4").css("visibility", "visible");
-	});
-	$("#q5").click(function(){
-		$("#a5").css("visibility", "visible");
-	});
-	$("#q6").click(function(){
-		$("#a6").css("visibility", "visible");
-	});
-	$("#q7").click(function(){
-		$("#a7").css("visibility", "visible");
-	});
-	$("#q8").click(function(){
-		$("#a8").css("visibility", "visible");
-	});
-	$("#q9").click(function(){
-		$("#a9").css("visibility", "visible");
-	});
-	$("#q10").click(function(){
-		$("#a10").css("visibility", "visible");
-	});
-	$("#q11").click(function(){
-		$("#a11").css("visibility", "visible");
+	$(".question").click(function(){
+		$(this).children().css("visibility", "visible");
 	});
 });
